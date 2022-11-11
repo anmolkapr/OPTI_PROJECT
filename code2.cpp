c3c4f971cb196eedb3a547b17be7a6b62170e0ac
Anmol Kapoor <83301287+anmolkapr@users.noreply.github.com>
2022-11-11 12:45:14 +0530

    Add files via upload

--- code2.cpp
+++ code2.cpp
@@ -9,34 +9,28 @@ vector<pair<string,int>> projectskills [10];
 int proj_bbfore[10];
 int proj_days[10];
 int numofproject= 3;
-map<string,int> skillmap;
+// map<string,int> skillmap;
 int counterskill ;
 bool optimised[3][3][3][10];
 int tot;
-
+vector<pair<int,int>> contributor[5];
 int digit[7];
 long int id=0;
-
+map<int,vector<pair<int,int>>> skillmap;
 void contributorInput(){
     
     cin >> numofcontri;
     int i;
     for(i =0 ; i < numofcontri ; i++){
         int nskills;
-        cin >> nskills;
-
+        cin >> nskills;                                                     
         int j;
         for(j = 0 ; j < nskills;j++){
-            string skillname;
-            int skillevel;
-            cin >> skillname >> skillevel;
-            contri[i].push_back({skillname,skillevel});
-            if(skillmap.count(skillname) == 0)
-            { 
-               skillmap[skillname] = counterskill;
-              counterskill++;
-            }
+            int skillcode, skilllevel;
+            cin >> skillcode >> skilllevel; 
+            contributor[i].push_back({skillcode,skilllevel});
         }
+
 
     }
 }
@@ -51,129 +45,137 @@ void projectInput(){
          cin >> skills;
          int j;
          for(j =0 ; j < skills;j++){
-            string skillname;
+            int skillcode;
             int skillevel;
-            cin >> skillname >> skillevel;
-            projectskills[i].push_back({skillname,skillevel});
-            if(skillmap.count(skillname) == 0)
-            { 
-               skillmap[skillname] = counterskill;
-              counterskill++;
-            }
+            cin >> skillcode >> skillevel;
+        //     projectskills[i].push_back({skillname,skillevel});
+        //     if(skillmap.count(skillname) == 0)
+        //     { 
+        //        skillmap[skillname] = counterskill;
+        //       counterskill++;
+        //     }
+        //  }
          }
     }
 
 }
 
-int randomarray[362881][9];
-int ctr ;
 
-void permute(string &a, int l, int r)
-{
-	if (l == r)
-		{
-      int i;
-      for(i = 0 ; i < a.length();i++){
-          randomarray[ctr][i] = a[i] - 'A';
-      }
-      ctr++;
-    } 
-	else
-	{
-		for (int i = l; i <= r; i++)
-		{
-      swap(a[l], a[i]);
-			permute(a, l+1, r);
-			swap(a[l], a[i]);
-		}
-	}
-}
-
-void allpermgenerator(){
-    string s = "ABCDEFGHI";
-    permute(s,0,9 - 1);
-}
-
-
-
-vector<int> randompermgenerator(int x){
-    
-    vector<int> v ;
-    int i;
-    for(i = 0 ;i < 9;i++){
-        v.push_back(randomarray[x][i]);
-    }
-    return v;
-}
-
-int randomstartpop[10001][9];
-
-void randompopulationgenerator(){
-    int i;
-    for(i =0 ; i < 10000;i++){
-        
-        digit[6]=rand()%29+1;
-
-        for (int i=5; i>=0; i--) {
-            digit[i]=rand()%10+1;
-        } 
-
-        long int x= digit[6]*pow(10,6) + digit[5]*pow(10,5) + digit[4]*pow(10,4) + digit[3]*pow(10,3)+digit[2]*pow(10,2) + digit[1]*10+digit[0]; 
-
-        x = x%362880;
-        
-        vector<int> ans = randompermgenerator(x);
-       
-        for(int j =0 ; j < 9;j++){
-            randomstartpop[i][j] = ans[j];
-        }
-
-    }
-}
 
 int main(){
-   
-
-    int i;
     counterskill = 0;
 
     // contributorInput();
     // projectInput();
-    int time = 10 ;
-    int skillsnum = skillmap.size();
-     tot = numofcontri + numofproject + skillsnum + time ;
-   
-
-    allpermgenerator();
-    
-    // for(i = 0 ;i < 362881;i++){
-       int j;
-       i = 362;
-    //    if(randomarray[i][0] == 1) cout << i << endl;;
-       for(j = 0 ; j < 9;j++){
-         cout << randomarray[i][j] ;
-       }
-       
-        cout << endl;
-    
-   
-    srand(static_cast<unsigned>(std::time(nullptr)));
-
-
-    //BEGININNING OF THE RANDOM POPULATION GENERATOR (METAHEURISTIC APPORACH )
-    randompopulationgenerator();
-
-    for(i = 0 ; i< 10000;i++){
-        int j;
-        for(j =0 ; j < 9;j++){
-            cout << randomstartpop[i][j] << ' ';
+    int i , j;
+    random_device rd;
+    mt19937 g(rd());
+    int vp100[10000][16];
+    int i , j;
+    for(i =0 ; i < 10000;i++){
+        for(j =0 ; j < 16;j++){
+            vp100[i][j] = j + 1;
         }
-        cout << endl;
     }
     
+    int rdmpopsize = 10000;
+    for ( i = 0; i < rdmpopsize; i++){
+        shuffle(vp100[i], vp100[i] + 16, g);
+    }
+
+    int t;
+    int iter;
+    
+    bool completed[16];
+    int skillreq[16];
+    int skillcodecorr[16];
+    int daysreq[16];
+    int bbf[16];
+    for(i = 0 ; i < 16;i++){
+        skillreq[i] = (i)%4 + 1;//required skill set of the project 
+        skillcodecorr[i] = i%5 + 1;// has the skill name that has to be awssociated with the code 
+        daysreq[i] = 5;
+        bbf[i] = i + 8;
+    }
+    
+   int contributors[5];
+   //numofcontrubutirs = 5
+   
+   for(i =0 ; i < 5;i++){
+      contributors[i] = 0;
+      
+   }
+    for(i = 0 ; i < 16;i++){
+       completed[i] = 0 ;
+    }
+
+    int contriava[5];\
+    int contritime[5];
+    for(i = 0 ; i < 5;i++){
+        contriava[i] = 0 ;
+        contritime[i] = 0;
+    }
+    
+    int iter ;
+    for(iter =0 ;iter < rdmpopsize;iter++){
+
+        int per[16] ;
+        int j;
+        for(j =0 ; j < 16;j++){
+            per[j] = vp100[i][j];
+        }
+
+        int time = 0;
+        for(time = 0; time < 10;time++)
+        {
+
+            // making contributor available 
+            for(j =0 ; j < 5;j++){
+                if(time == contritime[j]){
+                    contriava[j] = 0;
+                }
+            }
+
+        for(j = 0 ; j < 16;j++){
+            if(completed[j])continue;
+            else { 
+                int subtask = per[j];
+                int x = skillreq[subtask];
+                int y = skillcodecorr[subtask];
+                int k;
+                int minel = INT_MIN;
+                int index = -1;
+                for(k = 0 ; k < 5;k++){
+                    if(contriava[k] == 0)
+                    {int l;
+                    for(l = 0 ; l < contributor[k].size();l++){
+                        if(contributor[k][l].first == y && contributor[k][l].second >= x){
+                            minel = contributor[k][l].second;
+                            index = k;
+                        }
+                    } }
+                } 
+                if(index != -1)
+                {
+                    contritime[index] += daysreq[subtask] ;
+                    contriava[index] = 1;
+                }
+                
+                
+
+                
+                
+            }
+        }}
+
+
+
+
+    }
+
 
     
-    
+
 
 
 
